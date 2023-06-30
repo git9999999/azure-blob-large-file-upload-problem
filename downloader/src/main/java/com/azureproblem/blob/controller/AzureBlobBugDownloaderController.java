7e3d61ee33943caac7d8fb745431a9d4c45b4949
Urban Krieg TAAKRUR5 <urban.krieg@swisscom.com>
2023-06-30 14:24:31 +0200

    added new method according to stackoverflow comment https://stackoverflow.com/questions/76575117/can-not-transfer-more-than-250mb-with-databufferutils-write-to-azure-blob-storag/76580753#76580753 but it did not solve the problem

--- downloader/src/main/java/com/azureproblem/blob/controller/AzureBlobBugDownloaderController.java
+++ downloader/src/main/java/com/azureproblem/blob/controller/AzureBlobBugDownloaderController.java
@@ -21,6 +21,7 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -106,7 +107,7 @@ public class AzureBlobBugDownloaderController {
 
     @GetMapping(path = "/trigger-generation-of-random-data-to-blobstore/{fileSizeInMb}")
     public void triggerGenerationOfRandomDataToBlobStore(@PathVariable int fileSizeInMb) {
-        log.info("triggerDownloadToFile");
+        log.info("triggerGenerationOfRandomDataToBlobStore");
         try {
             byte[] bytes = new byte[fileSizeInMb * Constants.MB];
             SecureRandom.getInstanceStrong().nextBytes(bytes);
@@ -130,7 +131,7 @@ public class AzureBlobBugDownloaderController {
 
     @GetMapping(path = "/trigger-download-to-blob/{fileSizeInMb}")
     public void triggerDownloadToBlob(@PathVariable int fileSizeInMb) {
-        log.info("triggerDownload");
+        log.info("triggerDownloadToBlob");
 
         var flux = this.webClient
             .get()
@@ -147,6 +148,51 @@ public class AzureBlobBugDownloaderController {
                 .map(DataBufferUtils::release)
                 .blockLast(Duration.ofHours(22));
             outputStream.flush();
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+
+        log.info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!   end download of {}", destination);
+    }
+
+
+    /**
+     * https://stackoverflow.com/questions/76575117/can-not-transfer-more-than-250mb-with-databufferutils-write-to-azure-blob-storag/76580753#76580753
+     */
+    @GetMapping(path = "/trigger-download-to-blob-2/{fileSizeInMb}")
+    public void triggerDownloadToBlob2(@PathVariable int fileSizeInMb) {
+        log.info("triggerDownloadToBlob2");
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        var flux = this.webClient
+            .get()
+            .uri("/serve-file/" + fileSizeInMb)
+            .accept(MediaType.APPLICATION_OCTET_STREAM)
+            .exchangeToFlux(clientResponse -> clientResponse.body(BodyExtractors.toDataBuffers()));
+
+        var destination = "TestDownloadToAzureBlobStorage" + System.currentTimeMillis() + ".pdf";
+
+        var blobClientTarget = this.containerClient.getBlobClient(destination);
+
+        try (var outputStream = blobClientTarget.getBlockBlobClient().getBlobOutputStream()) {
+            DataBufferUtils.write(flux, outputStream)
+                .doOnError(throwable -> {
+                    // Handle the error gracefully
+                    stopWatch.stop();
+                    System.out.println("Error occurred during data streaming: " + throwable.getMessage());
+                })
+                .doFinally(signal -> {
+                    try {
+                        outputStream.flush();
+                        outputStream.close();
+                        stopWatch.stop();
+                        System.out.println("File uploaded successfully " + stopWatch.getTotalTimeMillis() + "ms");
+                    } catch (IOException e) {
+                        System.out.println("Error occurred while closing the output stream: " + e.getMessage());
+                    }
+                })
+                .blockLast(Duration.ofSeconds(111111111));
         } catch (IOException e) {
             throw new IllegalStateException(e);
         }
